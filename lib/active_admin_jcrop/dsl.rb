5fd7e1b0f4f6eb44aaa3a266dbc93b3c46134917
João Rosa <jp.rosa@campus.fct.unl.pt>
2019-10-17 00:16:03 +0100

    Update dsl.rb

--- lib/active_admin_jcrop/dsl.rb
+++ lib/active_admin_jcrop/dsl.rb
@@ -3,7 +3,7 @@ module ActiveAdminJcrop
     
     def jcropable
       member_action :jcropper, method: :put do
-        resource.active_admin_crop! params[:image_data]
+        Asset.active_admin_crop! params[:image_data]
         
         respond_to do |format|
           format.js { render json: true }
