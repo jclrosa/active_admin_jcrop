139057ddd1b31d9367af61f0fc153796a1b982ef
João Rosa <jp.rosa@campus.fct.unl.pt>
2019-10-17 17:57:50 +0100

    Update view_helpers.rb

--- lib/active_admin_jcrop/view_helpers.rb
+++ lib/active_admin_jcrop/view_helpers.rb
@@ -4,7 +4,7 @@ module ActiveAdminJcrop
     def jcropper_url(object)
       partial_url = []
       partial_url << ActiveAdmin.application.default_namespace
-      partial_url << object.assets.first.class.to_s.demodulize.underscore
+      partial_url << object.class.to_s.demodulize.underscore
 
       send("jcropper_#{partial_url.reject(&:blank?).join('_')}_path", object)
     end
